7540175b1e24d804dd414bac510b9009e68c10f1
maheshk.i4consulting <maheshk.i4consulting@gmail.com>
2024-07-09 18:18:22 +0530

    himanshu branch merged

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash 
 
 sed -i 's/\[]/\["184.72.78.226"]/' /home/ubuntu/nimbo-server/nimboproject/settings.py
-
+cd home/ubuntu/nimbo-server/nimboproject
 python3 manage.py migrate 
 python3 manage.py makemigrations     
 python3 manage.py collectstatic
