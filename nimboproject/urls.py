adc6cc0b576ed0594bda9b13e0c08fb844f3bd32
maheshk.i4consulting <maheshk.i4consulting@gmail.com>
2024-07-09 17:20:53 +0530

    himanshu branch merged

--- nimboproject/urls.py
+++ nimboproject/urls.py
@@ -2,15 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path,include 
-from nimboapp.views import GraphQLView, csrf_token_view
 from django.shortcuts import redirect
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-#  #   path('api/nimboql/', GraphQLView.as_view(), name='graphql'),
-#     path('csrf_token/', csrf_token_view, name='csrf_token'),
-#   #  path('', include('nimboapp.urls')),
-#     path('', lambda request: redirect('/admin/login/', permanent=True)),
-#     path('', include('nimboapp.urls')),
+
  ]
