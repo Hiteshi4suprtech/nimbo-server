065b91101029a28e74ae2476d3650d0454024007
maheshk.i4consulting <maheshk.i4consulting@gmail.com>
2024-07-09 20:08:52 +0530

    himanshu branch merged

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
 # Change directory to the home directory
-cd /home/ubuntu/
+cd /home/ubuntu/nimbo-server/
 
-# Create and activate a virtual environment
-python -m venv venv
-
-source /home/ubuntu/venv/bin/activate
-cd /home/ubuntu/
-# Install Django and other dependencies
-python -m pip install Django
-# Add any other dependencies you might have
-
-# Set PYTHONPATH to include your Django project directory
-
-# Change directory to your Django project directory
-cd /nimbo-server/
 pip install -r requirements.txt
 # Perform Django migrations
 python manage.py migrate
