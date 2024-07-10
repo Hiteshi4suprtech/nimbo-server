51291166fa6a16e3738c62dede90dd493e05e1f3
maheshk.i4consulting <maheshk.i4consulting@gmail.com>
2024-07-10 10:25:26 +0530

    himanshu branch merged

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -15,10 +15,19 @@ python manage.py collectstatic
 # # Make Django migrations if needed
 # python manage.py makemigrations
 
-ps aux | grep gunicorn
-# Collect static files
-
-
-# Restart gunicorn and nginx
-sudo systemctl restart gunicorn
+sudo systemctl status nginx.service
+sudo nginx -t
 sudo systemctl restart nginx
+sudo systemctl status nginx.service
+sudo lsof -i :80
+sudo lsof -i :443
+sudo journalctl -xeu nginx.service
+
+
+# ps aux | grep gunicorn
+# # Collect static files
+
+
+# # Restart gunicorn and nginx
+# sudo systemctl restart gunicorn
+# sudo systemctl restart nginx
