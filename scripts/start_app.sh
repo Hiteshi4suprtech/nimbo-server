7ef364f00c6299bf485690dd2c47f83dacf06402
maheshk.i4consulting <maheshk.i4consulting@gmail.com>
2024-07-10 10:17:08 +0530

    himanshu branch merged

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+sudo mkdir -p /home/ubuntu/nimbo-server/staticfiles
+sudo chown -R ubuntu:ubuntu /home/ubuntu/nimbo-server/staticfiles
+sudo chmod -R 755 /home/ubuntu/nimbo-server/staticfiles
 
 
 source /home/ubuntu/envnimbo/bin/activate
 
 cd /home/ubuntu/nimbo-server/
 
-
+python manage.py collectstatic
 # Perform Django migrations
 python manage.py migrate
 
@@ -14,7 +17,7 @@ python manage.py makemigrations
 
 ps aux | grep gunicorn
 # Collect static files
-python manage.py collectstatic --noinput
+
 
 # Restart gunicorn and nginx
 sudo systemctl restart gunicorn
