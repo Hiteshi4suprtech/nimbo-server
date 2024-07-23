8eed993625f0fe1fa640f9d708408c19d14d0926
maheshk.i4consulting <maheshk.i4consulting@gmail.com>
2024-07-23 17:07:54 +0530

    build 182 released

--- nimbo/settings.py
+++ nimbo/settings.py
@@ -51,6 +51,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # settings.py
 
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.hostinger.com'
 EMAIL_PORT = 587
